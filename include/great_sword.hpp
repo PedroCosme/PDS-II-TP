66a47a0d6257011c1a4572af1de9561c75a60c0d
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-23 20:32:04 -0300

    issue #5

---
+++ include/great_sword.hpp
@@ -0,0 +1,16 @@
+#ifndef GREAT_SWORD_H
+#define GREAT_SWORD_H
+#include "Equipment.hpp"
+
+/**
+ * @brief Subclasse da classe Equipment
+ */
+class GreatSword: public Equipment{
+
+
+
+
+
+
+}
+#endif
