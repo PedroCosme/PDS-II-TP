39358ceaf3478ed97fe1667d36c3618d0d4a9fa3
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-23 20:15:11 -0300

    issue #5

---
+++ include/long_sword.hpp
@@ -0,0 +1,16 @@
+#ifndef LONG_SWORD_H
+#define LONG_SWORD_H
+#include "equipament.hpp"
+
+/**
+ * @brief Subclasse da classe Equipament
+ */
+class LongSword: public Equipament{
+
+
+
+
+
+
+}
+#endif
