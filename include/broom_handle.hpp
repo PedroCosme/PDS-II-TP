d0651056d4c01e910d3e0a0b47b6b790b758edb5
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-23 21:42:57 -0300

    issue #5

---
+++ include/broom_handle.hpp
@@ -0,0 +1,16 @@
+#ifndef BROOM_HANDLE_H
+#define BROOM_HANDLE_H
+#include "equipment.hpp"
+
+/**
+ * @brief Subclasse da classe Equipment
+ */
+class BroomHandle: public Equipment{
+
+
+
+
+
+
+}
+#endif
