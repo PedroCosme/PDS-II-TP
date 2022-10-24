12c80d478f1a432eb0cc40baf475215f6c1596a0
Bruno Cimbleris <bcimbleris@gmail.com>
2022-10-23 21:38:13 -0300

    adiciona documentação doxygen #4

--- include/warrior.hpp
+++ include/warrior.hpp
@@ -8,5 +8,10 @@
  * @brief Classe responsável pela classe de personagem warrior.
  */
 class Warrior : public PlayableCharacter{
+   public:
+/**
+ * @brief construtor padrão para a classe de personagem warrior.
+ */
+    Warrior ();
 };
 #endif
