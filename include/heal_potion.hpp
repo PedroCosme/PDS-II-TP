7d75f552edbc612b2b148b255d4fdca984a421e5
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-23 22:06:30 -0300

    issue #5

---
+++ include/heal_potion.hpp
@@ -0,0 +1,21 @@
+#ifndef HEAL_POTION_H
+#define HEAL_POTION_H
+#include "equipment.hpp"
+
+/**
+ * @brief Subclasse da classe Equipment
+ */
+class HealPotion: public Equipment{
+public:
+/**
+ * @brief construtor padrão da classe
+ */
+HealPotion();
+/**
+ * @brief cura o personagem.
+ */
+float heal();
+
+
+}
+#endif
