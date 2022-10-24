92f26ec0c7a94c846b2fff3a14cf5c76afc8116c
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-23 21:53:15 -0300

    issue #5

---
+++ include/ancient_staff.hpp
@@ -0,0 +1,19 @@
+#ifndef ANCIENT_STAFF_H
+#define ANCIENT_STAFF_H
+#include "equipment.hpp"
+
+/**
+ * @brief Subclasse da classe Equipment
+ */
+
+class AncientStaff: public Equipment{
+  
+  /**
+ * @brief construtor padrão da classe
+ */
+
+  public:
+  AncientStaff();
+  
+}
+#endif
