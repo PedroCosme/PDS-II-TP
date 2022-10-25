3f875eaa389f1032dc35e72278a81d9374306fbc
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-10-25 20:52:25 -0300

    Create bandit.hpp

---
+++ include/bandit.hpp
@@ -0,0 +1,37 @@
+
+#ifndef _BANDIT_H
+#define _BANDIT_H
+
+#include "Monster.hpp"
+
+
+class Bandit: public Monster {
+public: 
+    
+float get_hp();
+    
+void set_hp();
+    
+Equipment drop_items();
+    
+void attack();
+    
+float hp_multiplier();
+    
+float take_damage();
+    
+void steal_item();
+    
+float hp_multiplier();
+    
+float take_damage();
+};
+
+
+private:
+       
+        float _hp();
+                    
+        int _difficulty();
+
+#endif 
