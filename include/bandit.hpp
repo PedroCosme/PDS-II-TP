600894dd66437da3c1118489234b014c513dc058
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-10-25 22:03:55 -0300

    Update bandit.hpp

--- include/bandit.hpp
+++ include/bandit.hpp
@@ -3,28 +3,49 @@
 #define _BANDIT_H
 
 #include "Monster.hpp"
+/**
+ * @brief Classe responsavel pelo Monster Bandit.
+ */
 
 
 class Bandit: public Monster {
 public: 
+  /**
+   * @brief Construtor padrao que inicializa todas as variaveis da classe.
+   * @param hp float com o hp base do jogador
+   * @param difficulty int que determina um multiplicador de dificuldade do goblin
+   * 
+   */
     
-    float get_hp();
-    
-    void set_hp();
+    Bandit(float hp, int difficulty);
     
     Equipment drop_items();
     
-    void attack();
+  /**
+   * @brief Retorna o dano do ataque do goblin
+   */
     
+    void attack();
+        
+  /**
+   * @return hp atual do goblin
+   */
     float hp_multiplier();
     
+  /**
+   * @brief retorna a quantidade de dano tomado pelo goblin
+   * @return float com o atual hp do personagem
+   */
+    
     float take_damage();
+    
+  /**
+   * @brief um item é retirado do personagem
+   * 
+   */
     
     void steal_item();
     
-    float hp_multiplier();
-    
-    float take_damage();
 
 
 
