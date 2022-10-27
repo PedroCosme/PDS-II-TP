6c031932562ad043f6faf5765c42e0288b5d75da
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-26 22:34:27 -0300

    issue #8
    correção de bug

--- include/broom_handle.hpp
+++ include/broom_handle.hpp
@@ -10,9 +10,9 @@ public:
   /**
  * @brief Construtor padrão para a classe.
  */
-  BroomHandle();
+  BroomHandle(std::string name, std::string allowed_class, int damage, float drop_rate);
 
 
 
-}
+};
 #endif
