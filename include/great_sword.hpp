f87321f386677d3a93ed468505d1cfabc173f3ba
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-10-26 23:46:42 -0300

    issue #8
    Adição de atributos herdados da superclasse Equipment ao construtor da classe GreatSword.

--- include/great_sword.hpp
+++ include/great_sword.hpp
@@ -1,16 +1,15 @@
 #ifndef GREAT_SWORD_H
 #define GREAT_SWORD_H
-#include "Equipment.hpp"
+#include "equipment.hpp"
 
 /**
  * @brief Subclasse da classe Equipment
  */
 class GreatSword: public Equipment{
-
-
-
-
-
-
+public:
+/**
+ * @brief Construtor padrão da classe
+ */
+GreatSword(std::string name, std::string allowed_class, int damage, float drop_rate);
 }
 #endif
