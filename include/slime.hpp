4e05b5429302594fdaa36abd5eb23b42020467b0
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-11-01 10:22:27 -0300

    Update slime.hpp

--- include/slime.hpp
+++ include/slime.hpp
@@ -3,12 +3,9 @@
 #define _SLIME_H
 
 #include "monster.hpp"
-
 /**
  * @brief Classe responsavel pelo Monster Slime.
  */
-
-
 class Slime: public Monster {
 public: 
 /**
@@ -17,7 +14,6 @@ public:
    * @param difficulty int que determina um multiplicador de dificuldade do slime
    * 
    */
-    
   Slime(float hp, int difficulty);
   
   float get_hp();
@@ -25,24 +21,18 @@ public:
   void set_hp();
     
   Equipment drop_items();
-
 /**
  * @brief Retorna o dano do ataque do slime
  */
-    
   void attack();
-
 /**
   * @return hp atual do slime
-  */
-    
+  */ 
   float hp_multiplier();
-
 /**
  * @brief Retorna a quantidade de dano tomado pelo slime
  * @return float com o atual hp do personagem
- */
-    
+ */    
   void take_damage();
     
   void root();
