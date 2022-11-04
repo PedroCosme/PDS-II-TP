cc2e448e53be01b9b5220d1e60604e7fc34fc880
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-04 20:19:25 -0300

    Update game_events.cpp
    Criação da função heal():float, que chama a função set_hp():void para atualizar o valor do hp do personagem, de tal forma que o novo hp passa a ser 25% do hp_inicial. Porém, para que isso ocorra, o hp imediatamente antes de tomar a healpotion deve ser menor do que hp_inicial*0.25, do contrário, a healpotion() teria o efeito contrário, ou seja, o hp do personagem diminuiria em relação ao atual.

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -1,9 +1,10 @@
 #include "game_events.hpp"
 
 void Game_events::heal(){
-  float novo_hp;
-  novo_hp=get_hp()*0.25;
-  set_hp(novo_hp);
+  
+  if(get_hp()<(hp_inicial*0.25)){
+  set_hp(hp_inicial*0.25);
+  }
 }
 void Game_events::game_end(){
   
