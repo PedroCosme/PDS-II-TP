59cb48a7c0d14db91e4b3ac79ff80ac583e7d0f7
Bruno Cimbleris <bcimbleris@gmail.com>
2022-11-14 16:34:45 -0300

    atualiza necromancer.cpp

---
+++ src/necromancer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <game_events.hpp>
+
+Necromancer::Necromancer(float hp, int mp, int difficulty){
+    this -> _hp = hp;
+    this -> _mp = mp;
+    this -> _difficulty = difficulty;
+}
