b334bcf3dddcc9db526a0408b038e3c8f023fd2d
Bruno Cimbleris <bcimbleris@gmail.com>
2022-11-14 16:20:55 -0300

    cria warrior.cpp

---
+++ src/warrior.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include "warrior.hpp"
+
+Warrior::Warrior(std::string name, float hp, float mp, int lvl, int xp, Equipment equipment, std::set<Equipment>& equipments){
+    this -> _hp = hp;
+    this -> _mp = mp;
+    this -> _lvl = lvl;
+    this -> _xp = xp;
+    this -> _equipment = equipment;
+    this -> _equipments = equipments;
+}
