30c1c3d6e56b7570bf91fcfc5c70d022ac45b4dc
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-11-16 15:01:06 -0300

    Update goblin.cpp

--- src/goblin.cpp
+++ src/goblin.cpp
@@ -3,21 +3,21 @@
 
 Goblin::Goblin (float hp, int difficulty):_hp(hp),_difficulty(difficulty);
 
-}
+
 
 void Goblin::set_hp (int hp){
     _hp += hp;
-    
+};    
 float Goblin::get_hp (){
     return _hp;
-    
+};     
 void Goblin::attack(){
     return attack;
-}
+}; 
 
 float Goblin::hp_multiplier(){
     return hp_multiplier;
-}
+}; 
 float Goblin::take_damage(){
     _hp -= 10 //valor não definido
-}
+}; 
