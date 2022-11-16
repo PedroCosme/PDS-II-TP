b452633ae16d9b002274fe6711c39bbc1f23e9d7
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-15 22:40:37 -0300

    Update ancient_staff.cpp
    implementação do construtor da classe.

--- src/ancient_staff.cpp
+++ src/ancient_staff.cpp
@@ -1,1 +1,10 @@
-Ancient_staff::Ancient_staff();
+#include "ancient_staff.hpp"
+
+AncientStaff::AncientStaff(std::string name, std::string allowed_class, int damage, float drop_rate){
+  
+  this->name=name;
+  this->allowed_class=allowed_class;
+  this->damage=damage;
+  this->drop_rate=drop_rate;
+  
+}
