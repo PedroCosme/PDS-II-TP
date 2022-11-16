67f87860a2631d4900aead9c436f048b0f4401de
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-11-16 00:59:03 -0300

    Create goblin.cpp

---
+++ src/goblin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "goblin.hpp"
+
+Goblin::Goblin (float hp, int difficulty):_hp(hp),_difficulty(difficulty);
+
+}
+
+void Goblin::set_hp (int hp){
+    _hp += hp;
+    
+float Goblin::get_hp (){
+    return _hp;
+    
+void Goblin::attack(){
+    return attack;
+}
+
+float Goblin::hp_multiplier(){
+    return hp_multiplier;
+}
+float Goblin::take_damage(){
+    _hp -= 10 //valor não definido
+}
