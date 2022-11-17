9e94970844886591e263c20772d267bcff61ec96
MarlonSoares <marlons.silva@hotmail.com>
2022-11-16 21:24:39 -0300

    Create monster.cpp

---
+++ src/monster.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include "monster.hpp"
+#include "game_events.hpp"
+
+
+Monster::Monster(float hp, int difficulty){
+    this -> _hp = hp;
+    this -> _difficulty = difficulty;
+};
+    float Monster::get_hp(){
+    return this->_hp;
+    
+};
+    int Monster::get_difficulty() {
+        retrun this ->_difficulty;
+};
+    void Monster::set_hp(float hp){
+    this->_hp += hp;
+}
