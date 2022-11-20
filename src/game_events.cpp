45a7b88e792a68d89cbaad57c51a6a98238dd20f
Gawlft <pcosme555@gmail.com>
2022-11-20 19:18:05 -0300

    Co-authored-by: Bruno Cimbleris <bcimbleris@users.noreply.github.com> Co-authored-by: Fernanda-Lara <Fernanda-Lara@users.noreply.github.com> Co-authored-by: marlons.silva@hotmail.com <marlons.silva@hotmail.com>

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -1,27 +1,43 @@
+#include <iostream>
+#include <string>
 #include "game_events.hpp"
+#include "mage.cpp"
+#include "warrior.cpp"
 
-void Game_events::heal(){
-  
-  if(get_hp()<(hp_inicial*0.25)){
-  set_hp(hp_inicial*0.25);
+void game_start()
+{
+  std::cout << "Bem vindo ao jogo, começaremos criando um personagem" << std::endl;
+  int choosen_class;
+  std::cout << "Primeiramente, escolha uma classe para o seu personagem. Digite 1 para jogar como mago e 2 como guerreiro" << std::endl;
+  std::cin >> choosen_class;
+  if (choosen_class == 1)
+  {
+  }
+  else if (choosen_class == 2)
+  {
+  }
+  {
   }
 }
-void Game_events::roll_dice(){
-  while(game_end()!=0){
-//aqui deve constar a função que mantém o jogo funcionando, por meio da geração de números aleatórios.//
-    
-    
-    
-    if(game_end()==0) std::cout<<"Game End"<<endl;
-     else if(game_end()==0)  break;
-}
-bool Game_events::game_end(){
-  if(get_hp()==0.0){
-    game_on=false;
-}
-  else{
-    game_on=true;
-  }
-  return(!game_on);
-}
-   
+// void Game_events::heal(){
+
+//   if(get_hp()<(hp_inicial*0.25)){
+//   set_hp(hp_inicial*0.25);
+//   }
+// }
+// void Game_events::roll_dice(){
+//   while(game_end()!=0){
+// //aqui deve constar a função que mantém o jogo funcionando, por meio da geração de números aleatórios.//
+
+//     if(game_end()==0) std::cout<<"Game End"<<endl;
+//      else if(game_end()==0)  break;
+// }
+// bool Game_events::game_end(){
+//   if(get_hp()==0.0){
+//     game_on=false;
+// }
+//   else{
+//     game_on=true;
+//   }
+//   return(!game_on);
+// }
