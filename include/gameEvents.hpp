ba005af1088cc46ae3b82448ac4a31cca1c6f85e
Bruno Cimbleris <bcimbleris@gmail.com>
2022-11-23 19:37:24 -0300

    adiciona funcoes novas a game events

---
+++ include/gameEvents.hpp
@@ -0,0 +1,21 @@
+#ifndef GAME_EVENTS_H
+#define GAME_EVENTS_H
+
+#include <iostream>
+#include <string>
+#include "playableCharacter.hpp"
+#include "monster.hpp"
+#include "enemy.hpp"
+#include "weapon.hpp"
+#include "item.hpp"
+#include "shop.hpp"
+
+class GameEvents{
+  public:
+  void battle(PlayableCharacter player, Monster monster);
+  unsigned int calcDamage(unsigned int damageRange);
+  unsigned int hitormiss();
+};
+
+
+#endif
