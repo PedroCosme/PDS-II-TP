fa73e79aade5e8c471882bf82935720b335573e6
Gawlft <pcosme555@gmail.com>
2022-11-26 11:36:14 -0300

    inventory methods

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -13,11 +13,32 @@ std::map<Item, int> Inventory::createInventory()
     Item dagger("Dagger", 50);
     Item longSword("Long sword", 120);
 
-    _inventory.insert({healthPotion, 1});
+    _inventory.insert({healthPotion, 2});
     _inventory.insert({grenade, 0});
     _inventory.insert({greatSword, 0});
     _inventory.insert({dagger, 1});
     _inventory.insert({longSword, 0});
 
     return _inventory;
+}
+
+std::map<Item, int> subtractItem(std::map<Item, int> inventory, Item item)
+{
+    if (inventory.at(item) > 0)
+    {
+        inventory.at(item) - 1;
+        return inventory;
+    }
+    else
+    {
+        std::cout << "You don't have enough of that item." << std::endl;
+    }
+}
+
+std::map<Item, int> addItem(std::map<Item, int> inventory, Item item)
+{
+
+    inventory.at(item) + 1;
+    std::cout << "You bought one" << item.getName() << std::endl;
+    return inventory;
 }
