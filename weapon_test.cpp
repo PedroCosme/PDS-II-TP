b88c4f1f31b42b7b4393e47182af9811d08ba614
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-28 20:14:38 -0300

    Update weapon_test.cpp
    Adição do teste do método getDamage:int

--- weapon_test.cpp
+++ weapon_test.cpp
@@ -23,3 +23,13 @@ TEST_CASE("testando getName()")
   CHECK_EQ(availableWeapons.at(WEAPONS::BATTLEAXE).getName(), "Battle Axe");
 
 }
+TEST_CASE("testando getDamage()")
+{
+  
+  CHECK_FALSE(availableWeapons.at(WEAPONS::UNARMED).getDamage()==3);
+  CHECK_FALSE(availableWeapons.at(WEAPONS::DAGGER).getDamage()==8);
+  CHECK_FALSE(availableWeapons.at(WEAPONS::LONGSWORD).getDamage()==0);
+  CHECK_FALSE(availableWeapons.at(WEAPONS::GREATSWORD).getDamage()==13);
+  CHECK_FALSE(availableWeapons.at(WEAPONS::BATTLEAXE).getDamage()==1);
+  
+}
