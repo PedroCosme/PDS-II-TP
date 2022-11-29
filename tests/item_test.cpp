57d3780d6e5054066a191112efd4c7a50360df38
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-29 00:44:16 -0300

    Update item_test.cpp
    A implementação do TEST_CASE de ambas as funções está pendente.

--- tests/item_test.cpp
+++ tests/item_test.cpp
@@ -1,1 +1,15 @@
+#include "doctest.h"
+#include "item.hpp"
+#include <iostream>
+
+TEST_CASE("getByName(std::string name): ITEMS")
+{
+  //implememtar
+}
+
+TEST_CASE(" bool operator<(const Item &rhs) const")
+{
+  //implememtar
+}
+
 
