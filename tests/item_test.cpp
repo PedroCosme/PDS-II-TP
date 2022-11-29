bc25488c212ce05f6bee8a796c73e1ed8f42844c
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-29 00:45:06 -0300

    Update item_test.cpp
    Teste da função getPrice( ):int

--- tests/item_test.cpp
+++ tests/item_test.cpp
@@ -11,5 +11,15 @@ TEST_CASE(" bool operator<(const Item &rhs) const")
 {
   //implememtar
 }
+TEST_CASE("getPrice(): int")
+{
+
+  CHECK(itemList.at(ITEMS::HEALTH_POTION).getPrice()==50);
+  CHECK(itemList.at(ITEMS::GRENADE).getPrice()==80);
+  CHECK(itemList.at(ITEMS::GREAT_SWORD).getPrice()==180);
+  CHECK(itemList.at(ITEMS::BATTLE_AXE).getPrice()==220);
+  CHECK(itemList.at(ITEMS::DAGGER).getPrice()==50);
+  CHECK(itemList.at(ITEMS::LONG_SWORD).getPrice()==120);
+}
 
 
