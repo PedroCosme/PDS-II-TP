5160b2fccc7b65b40eb7c754b4f77e8ec6a07518
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-11-28 21:50:37 -0300

    Update monster_test.cpp

--- tests/monster_test.cpp
+++ tests/monster_test.cpp
@@ -1,1 +1,20 @@
+#include "doctest.h"
+#include "monster.hpp"
+#include <iostream>
 
+TEST_CASE("isAlive():bool function, case currentHP==0")
+{
+Monster MONSTER("MONSTER", 0);
+  CHECK(MONSTER.isAlive()==0);
+
+}
+TEST_CASE("isAlive():bool function, case currentHP!=0")
+{
+  Monster MONSTER1("MONSTER", 58);
+  Monster MONSTER2("MONSTER", 2);
+  Monster MONSTER3("MONSTER", 1000);
+  CHECK_FALSE(MONSTER1.isAlive()==0);
+  CHECK_FALSE(MONSTER2.isAlive()==0);
+  CHECK_FALSE(MONSTER3.isAlive()==0);
+
+}
