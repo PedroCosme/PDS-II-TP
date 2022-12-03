43efede70ec709e938e8b2d26db7d38926e413bc
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-12-03 12:01:15 -0300

    Update enemy.hpp

--- include/enemy.hpp
+++ include/enemy.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file enemy.hpp
+ * @author Victória Tejada (victoriatsiqueiram@gmail.com)
+ * @brief Classe principal com a lógica de enemy
+ * @version 1.0
+ * @date 2022-12-03
+ * @details Responsável por processar e armazenar toda logica dos inimigos com atributos vindos dos monstros. 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef _ENEMY_HPP_
 #define _ENEMY_HPP_
 
