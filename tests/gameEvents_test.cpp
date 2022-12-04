80bbd2f3a8c3e99a7c3e8e42ee422f1ca69141be
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 15:14:53 -0300

    Update gameEvents_test.cpp

--- tests/gameEvents_test.cpp
+++ tests/gameEvents_test.cpp
@@ -1,1 +1,9 @@
+#include "doctest.h"
+#include "gameEvents.hpp"
+#include "inventory.hpp"
+#include <iostream>
+#include <sstream>
 
+TEST_CASE("Test battle function"){
+  PlayableCharacter P("David",  1,  0,  100, 100, 50, 1000);
+  Enemy monster(
