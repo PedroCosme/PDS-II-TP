a36d7b2d707abdee94e8e3353bd1a64f2926a36f
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 16:33:22 -0300

    Update gameEvents_test.cpp
    Test hitorMissMonster() function

--- tests/gameEvents_test.cpp
+++ tests/gameEvents_test.cpp
@@ -1,11 +1,17 @@
 #include "gameEvents.hpp"
 #include "doctest.h"
 
-TEST_CASE("Test hitorMissPlayer() function"){
+TEST_CASE("Test hitorMissPlayer() function")
+{
   GameEvents game;
   CHECK_FALSE(game.hitOrMissPlayer()==201);
   CHECK_FALSE(game.hitOrMissPlayer()==50);
 }
-}
 
+TEST_CASE("Test hitorMissMonster() function")
+{
+  GameEvents game;
+  CHECK_FALSE(game.hitOrMissPlayer()==101);
+  CHECK_FALSE(game.hitOrMissPlayer()==0);
+}
 
