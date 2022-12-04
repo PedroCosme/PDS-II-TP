e49ca28ecc80cd786bc6c0223b85c8a9661ea2ab
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 15:18:54 -0300

    Update gameEvents_test.cpp

--- tests/gameEvents_test.cpp
+++ tests/gameEvents_test.cpp
@@ -6,4 +6,9 @@
 
 TEST_CASE("Test battle function"){
   PlayableCharacter P("David",  1,  0,  100, 100, 50, 1000);
-  Enemy monster(
+  
+  Enemy monster(20, 50, "Goblin", 100);
+  battle(PlayableCharacter P, Enemy monster);
+//Implementar o resto
+  
+}
