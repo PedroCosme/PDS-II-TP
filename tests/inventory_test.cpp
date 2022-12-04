c12609f0b573f3ca43259706568b8a448f77b4f6
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 12:50:19 -0300

    Update inventory_test.cpp
    Testing subtractItem function after creating  the inventory map.

--- tests/inventory_test.cpp
+++ tests/inventory_test.cpp
@@ -1,1 +1,15 @@
+#include "doctest.h"
+#include "enemy.hpp"
+#include "inventory.hpp"
+#include <iostream>
 
+TEST_CASE("Test createInventory and subtractItem")
+{
+  Inventory inventory;
+  inventory.createInventory();
+  //consider that initially the inventory contains 2 Health Potions
+ inventory.subtractItem(itemList.at(ITEMS::HEALTH_POTION));
+  CHECK(inventory._inventory[itemList.at(ITEMS::HEALTH_POTION)]==1);
+  inventory.subtractItem(itemList.at(ITEMS::HEALTH_POTION));
+  CHECK(inventory._inventory[itemList.at(ITEMS::HEALTH_POTION)]==0);
+}
