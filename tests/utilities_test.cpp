3c697afc5a208cc463671b1224d104a5e7bd665a
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 08:27:14 -0300

    Update utilities_test.cpp

--- tests/utilities_test.cpp
+++ tests/utilities_test.cpp
@@ -6,6 +6,13 @@ TEST_CASE("Testing max funcion"){
   CHECK(max(29, 20)==29);
 }
 TEST_CASE("Testing min funcion"){
+    
+SUBCASE("positive"){
+  CHECK(min(9, 8)==8);
+  CHECK(min(29, 20)==20);
+}
+  SUBCASE("negative"){
   CHECK_FALSE(min(9, 8)==9);
   CHECK_FALSE(min(29, 20)==29);
+  }
 }
