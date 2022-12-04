adddec1daa671b32878586092bd60b3fbf1d9d49
Bruno Cimbleris <bcimbleris@gmail.com>
2022-12-04 10:41:48 -0300

    acrescenta funcao de compra

--- src/shop.cpp
+++ src/shop.cpp
@@ -72,3 +72,32 @@ int buyItem(int itemId)
         break;
     }
 }
+void checkBuy(char c, PlayableCharacter player, Inventory inventory){
+while (c == 's')
+    {
+        std::cout << "COMECO DO LOOP" << std::endl;
+
+        int desiredItem;
+        std::string desiredItemStr;
+        std::cout << "What are you buying, stranger?" << std::endl;
+        std::cin >> desiredItem;
+
+        int itemPrice = buyItem(desiredItem);
+        player.subtractGold(itemPrice);
+        desiredItemStr = getItemById(desiredItem);
+        inventory.addItem(desiredItemStr);
+        inventory.displayInventory();
+        std::cout << "FIM DO LOOP" << std::endl;
+        std::cout << "Do you want to buy something else? (y/n)" << std::endl;
+        char yn;
+        std::cin >> yn;
+        if ( yn == 'y'){
+            continue;
+        }
+        else{
+            
+            std::cout << "Press (Enter) to continue" << std::endl;
+            break;
+        }
+    }
+}
