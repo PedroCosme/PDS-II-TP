daf1221b6f2861520c0c823193f385ac1e9c497b
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 08:16:51 -0300

    Update utilities_test.cpp
    Test min(int a, int b):int function

--- tests/utilities_test.cpp
+++ tests/utilities_test.cpp
@@ -5,3 +5,7 @@ TEST_CASE("Testing max funcion"){
   CHECK(max(9, 8)==9);
   CHECK(max(29, 20)==29);
 }
+TEST_CASE("Testing min funcion"){
+  CHECK_FALSE(max(9, 8)==9);
+  CHECK_FALSE(max(29, 20)==29);
+}
