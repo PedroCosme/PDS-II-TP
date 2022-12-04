4faea2d01aa8af662e5eee7c53be42b8ae5b8312
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 15:51:53 -0300

    Update gameEvents_test.cpp
    no test required for battle function

--- tests/gameEvents_test.cpp
+++ tests/gameEvents_test.cpp
@@ -4,11 +4,4 @@
 #include <iostream>
 #include <sstream>
 
-TEST_CASE("Test battle function"){
-  PlayableCharacter P("David",  1,  0,  100, 100, 50, 1000);
-  
-  Enemy monster(20, 50, "Goblin", 100);
-  battle(PlayableCharacter P, Enemy monster);
-//Implementar o resto
-  
-}
+
