6c6f35738938b945cdb39cff4708a1736a56ce53
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-05 08:39:44 -0300

    Test getCurrentHp when heal( ):void is called

--- tests/playableCharacter_test.cpp
+++ tests/playableCharacter_test.cpp
@@ -9,15 +9,20 @@ TEST_CASE("Test getName():std::string function:")
 }
 
 
-TEST_CASE("Test getName():std::string function:")
+TEST_CASE("Test getCurrentHp():int function:")
 {
  PlayableCharacter player("Mike", 1, 0, 100, 100, 50, 1000);
   CHECK(player.getCurrentHp()==100);
   player.takeDamage(50);
   CHECK(player.getCurrentHp()==50);
+  player.heal(10);
+  CHECK(player.getCurrentHp()==60);
+ //if heal amount is bigger than the maxHp -> currentHp==maxHp
+  player.heal(300);
+  CHECK(player.getCurrentHp()==100);
   
 }
-  TEST_CASE("Test getLvl():std::string function:")
+  TEST_CASE("Test getLvl():int function:")
 {
  PlayableCharacter player("Mike", 1, 0, 100, 100, 50, 1000);
   CHECK(player.getLvl()==1);
