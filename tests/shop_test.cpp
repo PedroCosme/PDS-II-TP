e20bc8730080372f0a271b123df219e2b20067f3
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-05 11:25:52 -0300

    Test byItem(int):int function

--- tests/shop_test.cpp
+++ tests/shop_test.cpp
@@ -2,10 +2,21 @@
 #include "doctest.h"
 #include "shop.hpp"
 
+//Item healthPotion("Health Potion", 50, 1);
+//Item grenade("Grenade", 80, 2);
+//Item dagger("Dagger", 50, 3);
+//Item longSword("Long sword", 120, 4);
+//Item greatSword("Great Sword", 180, 5);
+//Item battleAxe("Battle Axe", 220, 6);
 
 TEST_CASE("Test buyItem( id):int function)
 {
-          
-          
-          
- }
+         
+  CHECK(buyItem(4)==50);
+  CHECK(buyItem(2)==80);
+  CHECK(buyItem(4)==120);
+  CHECK(buyItem(5)==180);
+  CHECK(buyItem(6)==220);
+
+}
+      
