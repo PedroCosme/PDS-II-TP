3b3e18508d2e4727346600d581b635754f432b98
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-05 12:16:24 -0300

    goldWorth( ):int and xpWorth( ):int tests

--- tests/enemy_test.cpp
+++ tests/enemy_test.cpp
@@ -1,1 +1,15 @@
+#include "doctest.h"
+#include "enemy.hpp"
+
+
+TEST_CASE("Test goldWorth():int")
+{  
+ Enemy bandit(50, 25, "Bandit", 50, 50, 15, 6);
+ CHECK(bandit.goldWorth()==50); 
+}
+TEST_CASE("Test xpWorth():int")
+{  
+ Enemy bandit(50, 25, "Bandit", 50, 50, 15, 6);
+ CHECK(bandit.xpWorth()==25); 
+}
 
