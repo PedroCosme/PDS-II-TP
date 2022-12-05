c1495bb592784509c867426fba3aa098bf8efee1
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-05 11:32:36 -0300

    Update shop_test.cpp

--- tests/shop_test.cpp
+++ tests/shop_test.cpp
@@ -12,11 +12,39 @@
 TEST_CASE("Test buyItem( id):int function)
 {
          
-  CHECK(buyItem(4)==50);
-  CHECK(buyItem(2)==80);
+  SUBCASE("Health Potion")
+  {
+  CHECK(buyItem(1)==50);
+  } 
+         
+         
+  SUBCASE("Grenade")
+  {
+   CHECK(buyItem(2)==80);
+  } 
+         
+         
+  SUBCASE("Dagger")
+  {
+       CHECK(buyItem(3)==50);
+  }
+         
+         
+  SUBCASE("Long Sword")
+  {
   CHECK(buyItem(4)==120);
+  }
+         
+         
+  SUBCASE("Great Sword")
+  {
   CHECK(buyItem(5)==180);
+  }
+     
+         
+  SUBCASE("Battle Axe")
+ {
   CHECK(buyItem(6)==220);
 
+  }
 }
-      
