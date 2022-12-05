0129777e2ada23bcbbc76e25bbde4b185ce328d6
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-05 12:33:13 -0300

    Update monster_test.cpp
    Alterar a chamada dos construtores de Monster que agora recebem 5 parâmetros em vez de 2.

--- tests/monster_test.cpp
+++ tests/monster_test.cpp
@@ -2,53 +2,55 @@
 #include "monster.hpp"
 #include <iostream>
 
-TEST_CASE("isAlive():bool function, case currentHP==0")
+TEST_CASE("isAlive():bool function, case currentHp==0")
 {
-  Monster MONSTER("MONSTER", 0);
-  CHECK(MONSTER.isAlive() == 0);
+  
+Monster MONSTER("MONSTER", 0, 50, 15, 6);
+  CHECK(MONSTER.isAlive()==0);
+
 }
-TEST_CASE("isAlive():bool function, case currentHP!=0")
+TEST_CASE("isAlive():bool function, case currentHp!=0")
 {
-  Monster MONSTER1("MONSTER", 58);
-  Monster MONSTER2("MONSTER", 2);
-  Monster MONSTER3("MONSTER", 1000);
-  CHECK_FALSE(MONSTER1.isAlive() == 0);
-  CHECK_FALSE(MONSTER2.isAlive() == 0);
-  CHECK_FALSE(MONSTER3.isAlive() == 0);
+  Monster MONSTER1("MONSTER", 1, 50, 15, 6);
+  Monster MONSTER2("MONSTER", 2, 50, 15, 6);
+  Monster MONSTER3("MONSTER", 3, 50, 15, 6);
+  CHECK(MONSTER1.isAlive()==1);
+  CHECK(MONSTER2.isAlive()==1);
+  CHECK(MONSTER3.isAlive()==1);
+
 }
-TEST_CASE("mtakeDamage:void function, case damage>currentHp")
+TEST_CASE("takeDamage:void function, case damage>currentHp")
 {
-  Monster MONSTER("MONSTER", 50);
+Monster MONSTER("MONSTER", 60, 80, 15, 6);
   MONSTER.takeDamage(60);
-  CHECK(MONSTER.isAlive() == 0);
+  CHECK(MONSTER.isAlive()==0);
+
 }
 TEST_CASE("getCurrentHp(): int")
 {
-  Monster MONSTER("MONSTER", 50);
+Monster MONSTER("MONSTER", 60, 70, 15, 6);
   MONSTER.takeDamage(30);
-  CHECK(MONSTER.getCurrentHp() == 20);
+  CHECK(MONSTER.getCurrentHp()==30);
+
 }
 TEST_CASE("currentHealth():std::string ")
 {
-  Monster MONSTER("MONSTER", 100);
-  CHECK(MONSTER.currentHealth() == "100 / ");
+Monster MONSTER("MONSTER", 90, 100, 15, 6);
+  CHECK(MONSTER.currentHealth()=="90 / 100 ");
   MONSTER.takeDamage(40);
-  CHECK_FALSE(MONSTER.currentHealth() == "100 / ");
+  CHECK_FALSE(MONSTER.currentHealth()=="50 / 100");
   MONSTER.takeDamage(30);
-  CHECK(MONSTER.currentHealth() == "30 / ");
+  CHECK(MONSTER.currentHealth()=="20 / 100");
+  
+  
 }
 
-TEST_CASE("ToString(): std::string ")
-{
-  Monster MONSTER("Goblin", 100);
-  CHECK(MONSTER.ToString() == "Goblin - Health: 100 / ");
-  MONSTER.takeDamage(40);
-  CHECK(MONSTER.ToString() == "Goblin - Health: 60 / ");
-}
 TEST_CASE("getName() const: std::string ")
 {
-  Monster MONSTER1("Bandit", 100);
-  CHECK(MONSTER1.getName() == "Bandit");
-  Monster MONSTER2("Vampire", 80);
-  CHECK(MONSTER2.getName() == "Vampire");
-}
+  Monster MONSTER1("Bandit", 30, 100, 15, 6);
+  CHECK(MONSTER1.getName()=="Bandit");
+  Monster MONSTER2("Vampire", 20, 100, 15, 6);
+  CHECK(MONSTER2.getName()=="Vampire");
+  
+  
+  }
