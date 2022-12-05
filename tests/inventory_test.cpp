779f72f1eebcbb843e5f492ff20c4ddde52de6b8
Fernanda-Lara <112732854+Fernanda-Lara@users.noreply.github.com>
2022-12-04 21:30:39 -0300

    Invalid option subcase-CHECK_THROWS

--- tests/inventory_test.cpp
+++ tests/inventory_test.cpp
@@ -12,6 +12,11 @@ TEST_CASE("Test createInventory and subtractItem")
   CHECK(inventory._inventory[itemList.at(ITEMS::HEALTH_POTION)]==1);
   inventory.subtractItem(itemList.at(ITEMS::HEALTH_POTION));
   CHECK(inventory._inventory[itemList.at(ITEMS::HEALTH_POTION)]==0);
+  //Implementar um try_catch em subtractItem(item):void
+  SUBCASE("Testing invalid option-substract item when _inventory[item]==0")
+{
+  CHECK_THROWS(inventory.subtractItem(itemList.at(ITEMS::HEALTH_POTION)));
+  }
 }
 
 TEST_CASE("Test createInventory and addItem")
@@ -23,4 +28,6 @@ TEST_CASE("Test createInventory and addItem")
   inventory.addItem(itemList.at(ITEMS::HEALTH_POTION));
   inventory.addItem(itemList.at(ITEMS::HEALTH_POTION));
   CHECK(inventory._inventory[itemList.at(ITEMS::HEALTH_POTION)]==5);
+  
 }
+    
