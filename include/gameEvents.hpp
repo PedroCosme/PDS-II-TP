8fe68893b4444dd9723c963665a406dfe79a1412
vicsiqueiram <victoriatsiqueiram@gmail.com>
2022-12-05 22:51:18 -0300

    documentacao

--- include/gameEvents.hpp
+++ include/gameEvents.hpp
@@ -28,13 +28,51 @@
 #include <random>
 #include <cmath>
 #include "inventory.hpp"
+
+/**
+ * @brief Especifica o hitOrMiss, calculo de dano e as batalhas.
+ * 
+ * @details Detalha o hitOrMiss, o calculo de dano e batalhas. Responsável por armazenar toda lógica presente no jogo.
+ */
+
 class GameEvents
 {
 public:
+
+/**
+ * @brief Construtor do objeto GameEvents
+ * 
+ * @details Inicializa a estrutura de GameEvents
+ */
   GameEvents();
+
+  /**
+   * @brief Destrói o objeto GameEvents
+   * 
+   */
   ~GameEvents();
+
+  /**
+   * @brief Batalha que acontece durante o jogo
+   * 
+   * @details Lida com a batalha que acontece durante o jogo, utilizando o jogador, monstros e inventário
+   * 
+   * @param player Jogador
+   * @param monster Monstro
+   * @param inventory Inventário
+   */
+
   void battle(PlayableCharacter player, Enemy monster, Inventory inventory);
+
+/**
+ * @brief 
+ * 
+ * @param damageRange 
+ * @return unsigned int 
+ */
   unsigned int calcDamage(unsigned int damageRange);
+
+
   int hitOrMissMonster();
   int hitOrMissPlayer();
 };
